b5284a1ee07314db5534086eaeb70af17169b715
Jason <jasontwuk@gmail.com>
2024-07-22 09:57:04 +0100

    refactor Dots.js and add notes

--- src/components/Dots.js
+++ src/components/Dots.js
@@ -6,6 +6,9 @@ const Dots = () => {
     const header = document.querySelector("header");
     const dotsContainer = document.getElementById("dots_container");
 
+    // *** return when header or dotsContainer not exist
+    if (!header || !dotsContainer) return;
+
     // !!!!!!!! VARIABLES !!!!!!!!
     const dotAmount = 100;
     const dotSize = 10;
@@ -16,10 +19,10 @@ const Dots = () => {
 
       newDot.classList.add("dot");
 
-      newDot.style.width = size + "px";
-      newDot.style.height = size + "px";
-      newDot.style.top = header.offsetHeight * Math.random() + "px";
-      newDot.style.left = header.clientWidth * Math.random() + "px";
+      newDot.style.width = `${size}px`;
+      newDot.style.height = `${size}px`;
+      newDot.style.top = `${header.offsetHeight * Math.random()}px`;
+      newDot.style.left = `${header.clientWidth * Math.random()}px`;
       // console.log(newDot);
       dotsContainer.appendChild(newDot);
     }
@@ -30,8 +33,12 @@ const Dots = () => {
     const dots = document.getElementsByClassName("dot");
 
     for (let i = 0; i < dots.length; i++) {
+      // *** make about 30% of the dots blink
       let boolean = Math.random() * 10 > 7;
+
+      // *** remove previous blink status
       dots[i].classList.remove("blink");
+
       if (boolean) {
         dots[i].classList.add("blink");
       }
@@ -43,29 +50,52 @@ const Dots = () => {
 
     // *** dots blinking effect
     const header = document.querySelector("header");
-    let addInterval = "";
+    let addInterval = null;
 
-    header.addEventListener("mouseenter", () => {
-      // console.log("start");
+    const startBlinking = () => {
+       // console.log("start");
+      dotBlink();
       addInterval = setInterval(dotBlink, 3000);
-    });
-    header.addEventListener("mouseleave", () => {
-      // console.log("stop");
-      clearInterval(addInterval);
-    });
+    };
+
+    const stopBlinking = () => {
+      if (addInterval) {
+        // console.log("stop");
+        clearInterval(addInterval);
+        addInterval = null;
+      }
+    };
+
+    if (header) {
+      header.addEventListener("mouseenter", () => {
+        startBlinking();
+      });
+      header.addEventListener("mouseleave", stopBlinking);
+    }
+
 
     // *** In Safari, there is a display delay when switching between dark-mode and light-mode, so I added the following event listener to the sunMoonContainer...
     const sunMoonContainer = document.getElementById("sun-moon-container");
-    sunMoonContainer.addEventListener("mouseenter", () => {
-      // console.log("stop");
-      // *** addInterval is from script.js
-      clearInterval(addInterval);
-    });
-    sunMoonContainer.addEventListener("mouseleave", () => {
-      // console.log("start");
-      // *** dotBlink is from script.js
-      addInterval = setInterval(dotBlink, 3000);
-    });
+
+    if (sunMoonContainer) {
+      sunMoonContainer.addEventListener("mouseenter", stopBlinking);
+      sunMoonContainer.addEventListener("mouseleave", startBlinking);
+    }
+
+    return () => {
+      if (header) {
+        header.removeEventListener("mouseenter", startBlinking);
+        header.removeEventListener("mouseleave", stopBlinking);
+      }
+
+      if (sunMoonContainer) {
+        sunMoonContainer.removeEventListener("mouseenter", stopBlinking);
+        sunMoonContainer.removeEventListener("mouseleave", startBlinking);
+      }
+
+      // *** Clear interval when the component unmounts
+      stopBlinking(); 
+    };
   }, []);
 
   return <div className="dots_container" id="dots_container"></div>;
